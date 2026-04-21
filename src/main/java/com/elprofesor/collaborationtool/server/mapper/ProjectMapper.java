89a2881a36e2486a7d5d58e789f8ac3fdf3c18a9
ElprofesorGuy <guyeinstein15@gmail.com>
2026-04-21 22:21:36 +0200

    Define mapper method for project entity and dtos

--- src/main/java/com/elprofesor/collaborationtool/server/mapper/ProjectMapper.java
+++ src/main/java/com/elprofesor/collaborationtool/server/mapper/ProjectMapper.java
@@ -1,16 +1,23 @@
 package com.elprofesor.collaborationtool.server.mapper;
 
 import com.elprofesor.collaborationtool.server.entities.Project;
-import com.elprofesor.collaborationtool.server.models.ProjectDTO;
+import com.elprofesor.collaborationtool.server.models.ProjectRequestDTO;
+import com.elprofesor.collaborationtool.server.models.ProjectResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper
 public interface ProjectMapper {
 
-    @Mapping(source = "owner_id", target = "owner.id")
-    Project projectDtoToProject(ProjectDTO projectDTO);
+    //@Mapping(source = "owner_id", target = "owner.id")
+    Project projectRequestDtoToProject(ProjectRequestDTO projectRequestDTO);
 
-    @Mapping(source = "owner.id", target = "owner_id")
-    ProjectDTO projectToProjectDto(Project project);
+    //@Mapping(source = "owner.id", target = "owner_id")
+    ProjectRequestDTO projectToProjectRequestDto(Project project);
+
+    @Mapping(source = "owner.email", target = "ownerEmail")
+    ProjectResponseDTO projectToProjectResponseDto(Project project);
+
+    @Mapping(source = "ownerEmail", target = "owner.email")
+    Project projectResponseDtoToProject(ProjectResponseDTO projectResponseDTO);
 }
