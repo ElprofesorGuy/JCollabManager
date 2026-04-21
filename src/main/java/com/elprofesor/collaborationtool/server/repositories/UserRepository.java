74da7721a486c56f15955d4b874ac40836911bac
ElprofesorGuy <guyeinstein15@gmail.com>
2026-04-21 22:20:36 +0200

    Add a new method : findByEmail

--- src/main/java/com/elprofesor/collaborationtool/server/repositories/UserRepository.java
+++ src/main/java/com/elprofesor/collaborationtool/server/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.elprofesor.collaborationtool.server.repositories;
 import com.elprofesor.collaborationtool.server.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<Users, UUID> {
@@ -10,5 +11,7 @@ public interface UserRepository extends JpaRepository<Users, UUID> {
 
     //Optional<Users> findByEmail(String email);
 
-    Users findByEmail(String email);
+    //Users findByEmail(String email);
+
+    Optional<Users> findByEmail(String email);
 }
