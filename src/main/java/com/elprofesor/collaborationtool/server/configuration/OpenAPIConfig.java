1635c8dbcbc4c1b7a984a6148ca345ad17a93b74
ElprofesorGuy <guyeinstein15@gmail.com>
2026-04-21 17:04:56 +0200

    Configuration class for API documentation

---
+++ src/main/java/com/elprofesor/collaborationtool/server/configuration/OpenAPIConfig.java
@@ -0,0 +1,33 @@
+package com.elprofesor.collaborationtool.server.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("JcollabManager API")
+                        .version("1.0")
+                        .description("API de gestion de projets collaboratifs")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
