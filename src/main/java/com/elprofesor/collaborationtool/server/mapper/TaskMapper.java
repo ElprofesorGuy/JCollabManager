aac063929058268a5f728d0fa9021f734fb6189a
ElprofesorGuy <guyeinstein15@gmail.com>
2026-04-22 06:04:50 +0200

    Define mapping method for task Class

--- src/main/java/com/elprofesor/collaborationtool/server/mapper/TaskMapper.java
+++ src/main/java/com/elprofesor/collaborationtool/server/mapper/TaskMapper.java
@@ -1,11 +1,24 @@
 package com.elprofesor.collaborationtool.server.mapper;
 
 import com.elprofesor.collaborationtool.server.entities.Task;
-import com.elprofesor.collaborationtool.server.models.TaskDTO;
+import com.elprofesor.collaborationtool.server.models.TaskRequestDTO;
+import com.elprofesor.collaborationtool.server.models.TaskResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface TaskMapper {
-    Task taskDtoToTask(TaskDTO taskDto);
-    TaskDTO taskToTaskDto(Task task);
+    @Mapping(source = "assign_to", target = "assign_to.username")
+    Task taskRequestDtoToTask(TaskRequestDTO taskRequestDto);
+
+    @Mapping(source = "assign_to.username", target = "assign_to")
+    TaskRequestDTO taskToTaskRequestDto(Task task);
+
+    @Mapping(source = "assign_to", target = "assign_to.username")
+    @Mapping(source = "projectName", target = "project.title")
+    Task taskResponseDtoToTask(TaskResponseDTO taskResponseDTO);
+
+    @Mapping(source = "assign_to.username", target = "assign_to")
+    @Mapping(source = "project.title", target = "projectName")
+    TaskResponseDTO taskToTaskResponseDto(Task task);
 }
