7efcac6490b90d0e79e79cf82ae8f0c430eb50ea
ElprofesorGuy <guyeinstein15@gmail.com>
2026-06-05 19:24:33 +0200

    fichier de configuration webMvc pour l'upload des fichiers

--- src/main/java/com/elprofesor/collaborationtool/server/configuration/WebMvcConfig.java
+++ src/main/java/com/elprofesor/collaborationtool/server/configuration/WebMvcConfig.java
@@ -3,11 +3,13 @@ package com.elprofesor.collaborationtool.server.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
