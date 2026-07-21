133dcf1a3465aa0dde6802885f885cde57972116
ElprofesorGuy <guyeinstein15@gmail.com>
2026-07-21 12:37:43 +0200

    feat(WorkflowStatus): gestion des tâches
    Personnalisation des statuts des tâches

--- src/main/java/com/elprofesor/collaborationtool/server/services/WorkflowStatusJPA.java
+++ src/main/java/com/elprofesor/collaborationtool/server/services/WorkflowStatusJPA.java
@@ -1,4 +1,44 @@
 package com.elprofesor.collaborationtool.server.services;
 
-public class WorkflowStatusJPA {
+import com.elprofesor.collaborationtool.server.mapper.WorkflowStatusMapper;
+import com.elprofesor.collaborationtool.server.models.WorkflowStatusRequestDTO;
+import com.elprofesor.collaborationtool.server.models.WorkflowStatusResponseDTO;
+import com.elprofesor.collaborationtool.server.repositories.WorkflowStatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class WorkflowStatusJPA implements WorkflowStatusService{
+
+    private final WorkflowStatusRepository workflowStatusRepository;
+    private final WorkflowStatusMapper mapper;
+
+    @Override
+    public WorkflowStatusResponseDTO addWorkflowStatus(WorkflowStatusRequestDTO workflowStatusRequestDTO) {
+         return mapper.workflowStatusToDto(workflowStatusRepository.save(mapper.workflowStatusRequestDtoToWorkflowStatus(workflowStatusRequestDTO)));
+    }
+
+    @Override
+    public List<WorkflowStatusResponseDTO> getWorkflowStatusOfProject(UUID projectId) {
+        return workflowStatusRepository.findByProjectIdOrderByOrderIndexAsc(projectId)
+                .stream()
+                .map(mapper::workflowStatusToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<WorkflowStatusResponseDTO> modifyWorkflowStatusOfProject(UUID id, WorkflowStatusRequestDTO requestDTO) {
+        return workflowStatusRepository.findById(id)
+                .map(workflowStatus -> {
+                    workflowStatus.setName(requestDTO.getName());
+                    return workflowStatusRepository.save(workflowStatus);
+                })
+                .map(mapper::workflowStatusToDto);
+    }
 }
