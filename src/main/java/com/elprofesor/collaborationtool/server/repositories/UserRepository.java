a7beff81039311188013301c353e4d254fdf37de
ElprofesorGuy <guyeinstein15@gmail.com>
2026-07-23 15:31:16 +0200

    fix(Utilisateur): Gestion utilisateur
    recherche des utilisateurs via email

--- src/main/java/com/elprofesor/collaborationtool/server/repositories/UserRepository.java
+++ src/main/java/com/elprofesor/collaborationtool/server/repositories/UserRepository.java
@@ -8,10 +8,5 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findByUsername(String username);
-
-    //Optional<Users> findByEmail(String email);
-
-    //Users findByEmail(String email);
-
     Optional<Users> findByEmail(String email);
 }
